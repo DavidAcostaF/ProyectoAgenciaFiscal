4be7a86b1919343a6c51f39ada6e4b4b362354c2
David <acostafajardodavid@gmail.com>
2024-03-19 15:33:47 -0700

    Creacion de las capas de presentacion y persistencia

---
+++ AgenciaFiscalDominio/target/generated-sources/annotations/com/mycompany/agenciafiscaldominio/Cliente_.java
@@ -0,0 +1,21 @@
+package com.mycompany.agenciafiscaldominio;
+
+import java.util.Calendar;
+import javax.annotation.processing.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-03-18T04:28:31", comments="EclipseLink-2.7.12.v20230209-rNA")
+@StaticMetamodel(Cliente.class)
+public class Cliente_ { 
+
+    public static volatile SingularAttribute<Cliente, Boolean> discapacitado;
+    public static volatile SingularAttribute<Cliente, String> apellido_paterno;
+    public static volatile SingularAttribute<Cliente, String> apellido_materno;
+    public static volatile SingularAttribute<Cliente, Calendar> fecha_nacimiento;
+    public static volatile SingularAttribute<Cliente, Long> id;
+    public static volatile SingularAttribute<Cliente, String> telefono;
+    public static volatile SingularAttribute<Cliente, String> nombre;
+    public static volatile SingularAttribute<Cliente, String> rfc;
+
+}
---
+++ AgenciaFiscalDominio/target/generated-sources/annotations/com/mycompany/agenciafiscaldominio/Placa_.java
@@ -0,0 +1,13 @@
+package com.mycompany.agenciafiscaldominio;
+
+import javax.annotation.processing.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-03-18T04:28:31", comments="EclipseLink-2.7.12.v20230209-rNA")
+@StaticMetamodel(Placa.class)
+public class Placa_ { 
+
+    public static volatile SingularAttribute<Placa, Long> id;
+
+}
