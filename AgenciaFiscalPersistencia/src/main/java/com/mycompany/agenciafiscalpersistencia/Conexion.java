75c3b5010c0e318c2668e7b2a70980827cc752e8
David <acostafajardodavid@gmail.com>
2024-03-19 19:20:34 -0700

    Daos creadas

---
+++ AgenciaFiscalPersistencia/src/main/java/com/mycompany/agenciafiscalpersistencia/Conexion.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.agenciafiscalpersistencia;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+/**
+ *
+ * @author af_da
+ */
+public class Conexion implements IConexion {
+
+    @Override
+    public EntityManager obtenerConexion() {
+        //Obtenemos acceso a la frabrica de entitymanagers
+        EntityManagerFactory emFactory = Persistence.createEntityManagerFactory("AgenciaFiscalPU");
+        // solicitamos una entity manager (acceso a la bd)
+        EntityManager entityManager = emFactory.createEntityManager();
+
+        return entityManager;
+    }
+
+}
---
+++ AgenciaFiscalPersistencia/src/main/java/com/mycompany/agenciafiscalpersistencia/IClienteDAO.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.mycompany.agenciafiscalpersistencia;
+
+import com.mycompany.agenciafiscaldominio.Cliente;
+
+/**
+ *
+ * @author af_da
+ */
+public interface IClienteDAO {
+    public Cliente agregar();
+}
